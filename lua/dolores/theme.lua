40e2df817dfc96bcbbaf4bd33e29ed7edc4942f9
Karavellas <yiannis.caravellas@gmail.com>
2026-08-25 23:26:58 +0300

    style: Bold on selected Visual mode

--- lua/dolores/theme.lua
+++ lua/dolores/theme.lua
@@ -142,7 +142,7 @@ local function set_highlights()
         TabLineSel = { fg = palette.text, bg = palette.c4, bold = styles.bold },
         Title = { fg = palette.c14, bold = styles.bold },
         VertSplit = { fg = groups.border },
-        Visual = { bg = palette.c12, blend = 30 },
+        Visual = { bg = palette.c12, bold = styles.bold, blend = 30 },
         YankHighlight = { fg = "NONE", bg = palette.c14, blend = 10 },
         WarningMsg = { fg = groups.warn, bold = styles.bold },
         WildMenu = { link = "IncSearch" },
